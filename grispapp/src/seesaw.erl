21112a4abe986996b0498f78e2ee1e4e033834f5
nextloop <nextloop@i2pmail.org>
2018-02-19 21:16:19 +0100

    Halfway done

--- grispapp/src/seesaw.erl
+++ grispapp/src/seesaw.erl
@@ -10,7 +10,7 @@
 
 -export([start_link/0,
 	 init/1,
-	 loop/6,
+	 loop/7,
 %	 reset/2,
 	 system_continue/3,
 	 system_terminate/4,
@@ -18,10 +18,13 @@
 
 -define(STOP_DEG, 7).
 -define(ROLL_DEG, 2).
--define(P_VAL_T, 1000*1000*300).
--define(P_VAL_A, 1000*1000*12).
+-define(P_VAL_T, 1000*1000*1000*135).
+-define(P_VAL_A, 1000*1000*40).
 -define(SCHWELLE, 1000*75).
 -define(CORR, 0.2).
+-define(TSOLL, 500*1000).
+%-define(C, 0.02).
+%-define(CASCADE_T, 0.
 
 start_link() -> proc_lib:start_link(?MODULE , init, [self()]).
 
@@ -58,21 +61,32 @@ reset(Parent, P, RollAngle, Init, StartAngle) ->
     timer:sleep(100),
     trinamic5130:move_to(degrees, 0),       
     trinamic5130:move_to(degrees, RollAngle + StartAngle),
-    ?MODULE:loop(Parent, P, links, erlang:monotonic_time(microsecond), 0, RollAngle).
+    ?MODULE:loop(Parent, P, links, erlang:monotonic_time(microsecond), 0, RollAngle, 400000).
+
+vorregelung(Diff, AvgDiff) ->
+    AvgDiffNew = Diff * 0.5 + AvgDiff * 0.5,
+    Ret = AvgDiffNew / ?TSOLL,
+    {Ret, AvgDiffNew}.
+    
 
 pcontrol_t(Val) ->
-    case Res = abs(round(?P_VAL_T / Val)) of
-	Res when Res =< 1000 -> Res;
+    case Res = abs(round(?P_VAL_T / math:sqrt(Val))) of
+	Res when Res =< 1000 ->
+	    io:fwrite("Sleeping for ~p~n", [Res]),
+	    Res;
 	Res -> 1000
     end.
 	     
 pcontrol_a(Val) ->
-    case Res = abs(round(?P_VAL_A / Val)) of
-	Res when Res =< 8 -> Res;
-	Res -> 8
+    case Res = abs(?P_VAL_A / math:sqrt(Val)) of
+	Res when Res =< 10 ->
+	    io:fwrite("Angle ~p~n", [Res]),
+	    Res;
+	Res -> 10
     end.
   
-loop(Parent, P, Richtung, Time, LastDiff, RollAngle) ->
+loop(Parent, P, Richtung, Time, LastDiff, RollAngle, AvgDiff) ->
+    io:fwrite("LastDiff ~p~n", [LastDiff]),
     receive
         %% If you enable trap_exit, you also want this clause.
 	reset ->
@@ -96,18 +110,20 @@ loop(Parent, P, Richtung, Time, LastDiff, RollAngle) ->
 			Diff - LastDiff =< -?SCHWELLE -> RollAngleNew = RollAngle - ?CORR;
 			true -> RollAngleNew = RollAngle
 		    end,
-		    trinamic5130:move_to(degrees, - pcontrol_a(Diff) + RollAngleNew),
-		    timer:sleep(pcontrol_t(Diff)),
+		    {VorR, AvgDiffNew} = vorregelung(Diff, AvgDiff),
+		    io:fwrite("Vorregler ~p ~n", [VorR]),
+		    trinamic5130:move_to(degrees, - pcontrol_a(Diff / VorR) + RollAngleNew),
+		    timer:sleep(pcontrol_t(Diff / VorR)),
 		    trinamic5130:move_to(degrees, - ?ROLL_DEG + RollAngleNew),
-		    grisp_ir_drv:activate_ir(P, spi1_pin9, 1), 
+		    grisp_ir_drv:activate_ir(P, spi1_pin9, 1),
 		    io:fwrite("Korrekur: ~p~n", [RollAngleNew]), 
 		    %grisp_ir_drv:activate_ir(P, spi1_pin10, 1),
-		    ?MODULE:loop(Parent, P, rechts, Time, Diff, RollAngleNew);
+		    ?MODULE:loop(Parent, P, rechts, Time, Diff, RollAngleNew, AvgDiffNew);
 
 		rechts ->
 		    Now = erlang:monotonic_time(microsecond),
 		    grisp_ir_drv:activate_ir(P, spi1_pin9, 1),
-		    ?MODULE:loop(Parent, P, rechts, Now, LastDiff, RollAngle)
+		    ?MODULE:loop(Parent, P, rechts, Now, LastDiff, RollAngle, AvgDiff)
 	    end;	    
 	{P, {data, <<20>>}} ->
 	    io:fwrite("Rechts~n"),
@@ -121,24 +137,26 @@ loop(Parent, P, Richtung, Time, LastDiff, RollAngle) ->
 			true -> RollAngleNew = RollAngle
 		    end,
 
-		    trinamic5130:move_to(degrees, pcontrol_a(Diff) + RollAngleNew),
-		    
-		    timer:sleep(pcontrol_t(Diff)),
+
+		    {VorR, AvgDiffNew} = vorregelung(Diff, AvgDiff),
+		    io:fwrite("Vorregler ~p ~n", [VorR]),
+		    trinamic5130:move_to(degrees, pcontrol_a(Diff / VorR) + RollAngleNew),
+		    timer:sleep(pcontrol_t(Diff / VorR)),
 		    trinamic5130:move_to(degrees, ?ROLL_DEG + RollAngleNew),
 		    grisp_ir_drv:activate_ir(P, spi1_pin10, 1),
 		    io:fwrite("Korrekur: ~p~n", [RollAngleNew]), 
 		    %grisp_ir_drv:activate_ir(P, spi1_pin9, 1),
-		    ?MODULE:loop(Parent, P, links, Time, Diff, RollAngleNew);
+		    ?MODULE:loop(Parent, P, links, Time, Diff, RollAngleNew, AvgDiffNew);
 		links ->
 		    Now = erlang:monotonic_time(microsecond),
 		    grisp_ir_drv:activate_ir(P, spi1_pin10, 1),
-		    ?MODULE:loop(Parent, P, links, Now, LastDiff, RollAngle)
+		    ?MODULE:loop(Parent, P, links, Now, LastDiff, RollAngle, AvgDiff)
 	    end;
 	Msg -> io:fwrite("Got message: ~p~n", [Msg]),
-	       ?MODULE:loop(Parent, P, Richtung, Time, LastDiff, RollAngle)
+	       ?MODULE:loop(Parent, P, Richtung, Time, LastDiff, RollAngle, AvgDiff)
     end.
 
-system_continue(_, _, {state, Parent, P, Richtung, Time, Diff, Angle}) -> loop(Parent, P, Richtung, Time, Diff, Angle).
+system_continue(_, _, {state, Parent, P, Richtung, Time, Diff, Angle, AvgDiff}) -> loop(Parent, P, Richtung, Time, Diff, Angle, AvgDiff).
 
 system_terminate(Reason, _, _, _) -> exit(Reason).
 
